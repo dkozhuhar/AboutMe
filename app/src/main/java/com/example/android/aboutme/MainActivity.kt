073322312ce6f33b37142f2bb085668fa4b1794b
Kozhukhar Dmitry <dna.coj@gmail.com>
2019-03-14 18:53:14 +0300

    Data binding 1

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: com.example.android.aboutme.databinding.ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //super.onCreate(savedInstanceState)
+        binding.setContentView(R.layout.activity_main)
+        val nickName = findViewById<TextView>(R.id.nickname_text)
+        val editNickName = findViewById<TextView>(R.id.editText)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            nickName.text = editNickName.text
+            it.visibility = View.GONE
+            editNickName.visibility = View.GONE
+            nickName.visibility = View.VISIBLE
+            // Hide the keyboard.
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(it.windowToken, 0)
+        }
     }
 }
